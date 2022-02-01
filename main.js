18245845b86ade2bcebe5cd9cf8a045302f655dd
MURAKI Taichi <muraki.taichi@gmail.com>
2022-02-01 19:40:31 +0900

    完成

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const ort = require('onnxruntime-web');
 
 import data from './template_params.json';
+import imagedata from './batch_imgs.json';
 
 const get_float_array = (arr) => {
     return Float32Array.from(arr);
@@ -17,16 +18,25 @@ const from_3dim_to_tensor = (arr, shape) => {
     return to_tensor(f32_arr, shape);
 }
 
+const from_4dim_to_tensor = (arr, shape) => {
+    const lis = arr.flat().flat().flat();
+    const f32_arr = get_float_array(lis);
+    return to_tensor(f32_arr, shape);
+}
+
 //template_3d_joints: torch.Size([1, 21, 3]) torch.float32
 //template_vertices_sub: torch.Size([1, 195, 3]) torch.float32
+//batch_imgs: torch.Size([1, 3, 224, 224])
 
 const load_input_data = () => {
     const joints = data["template_3d_joints"];
     //const betas = from_3dim_to_tensor(d, [1, 21, 3]);
     const vertices = data["template_vertices_sub"];
+    const img = imagedata["batch_imgs"];
     const ret = {
         template_3d_joints :from_3dim_to_tensor(joints, [1, 21, 3]),
         template_vertices_sub:from_3dim_to_tensor(vertices, [1, 195, 3]),
+        batch_imgs: from_4dim_to_tensor(img, [1, 3, 224, 224])
     };
     console.log(ret);
     return ret;
@@ -43,11 +53,12 @@ async function main() {
         const pred_camera = results.pred_camera;
         const pred_vertices = results.pred_vertices;
         console.log(pred_camera);
+        console.log(pred_vertices);
         //pred_camera, pred_3d_joints, pred_vertices_sub, pred_vertices
-        // document.write(`vertex[dims]: ${vertices.dims}<br>`);
-        // document.write(`vertex[size]: ${vertices.size}<br>`);
-        // document.write(`faces[dims]: ${faces.dims}<br>`);
-        // document.write(`faces[size]: ${faces.size}<br>`);
+        document.write(`pred_camera[dims]: ${pred_camera.dims}<br>`);
+        document.write(`pred_camera[size]: ${pred_camera.size}<br>`);
+        document.write(`pred_vertices[dims]: ${pred_vertices.dims}<br>`);
+        document.write(`pred_vertices[size]: ${pred_vertices.size}<br>`);
         // //console.log(vertices);
         // const num = 778*3;
         // console.log(num);
