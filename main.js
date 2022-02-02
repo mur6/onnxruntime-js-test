3490355145428d55b0b45d6c8c5fee3829e8a8ac
MURAKI Taichi <muraki.taichi@gmail.com>
2022-02-02 11:51:18 +0900

    Fix to use webcam and video element.

--- main.js
+++ main.js
@@ -1,7 +1,52 @@
-import {initModel} from './model.js';
+//import {initModel} from './model.js';
+
+function draw() {
+    // fill vertically
+    var vratio = (c.height / v.videoHeight) * v.videoWidth;
+    ctx.drawImage(v, 0, 0, vratio, c.height);
+    // fill horizontally
+    var hratio = (c.width / v.videoWidth) * v.videoHeight;
+    ctx1.drawImage(v, 0, 0, c1.width, hratio);
+    requestAnimationFrame(draw);
+}
+
+//     function updateCanvas() {
+//     context.drawImage(videoEl, 0, 0, canvasEl.width, canvasEl.height);
+//     window.requestAnimationFrame(updateCanvas);
+//   }
+//   requestAnimationFrame(updateCanvas);
+async function getMediaAndSetStream(video) {
+    const constraints = { audio: false, video: true }
+    //let stream = null;
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = stream
+    } catch (err) {
+        document.write(`failed to use getUserMedia: ${err}.`);
+    }
+}
 
 async function main() {
-    initModel();
+    const canvas = document.createElement('canvas');
+    canvas.width = 200;
+    canvas.height = 200;
+
+    var ctx = canvas.getContext('2d');
+
+    //var c1 = c.cloneNode(true);
+    //var ctx1 = c1.getContext('2d')
+
+    const video = document.createElement('video');
+    video.autoplay = true;
+    getMediaAndSetStream(video);
+
+    document.body.appendChild(document.createTextNode('fill vertical\n'));
+    document.body.appendChild(canvas);
+
+    document.body.appendChild(document.createTextNode('original video'));
+    document.body.appendChild(video);
+
+
 }
 
 main()
